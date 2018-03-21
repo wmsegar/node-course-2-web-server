1af4f9b15d718317553ab66e21ca22495c0daa80
wmsegar <wayne.segar@gmail.com>
2018-03-21 18:50:42 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use(function(req, res, next){
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', function(error){
+    if (error) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use(function(req, res, next){
+//   res.render('maintenance.hbs');
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', function(){
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function(text){
+  return text.toUpperCase();
+});
+
+app.get('/', function(req, res){
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hello World',
+  });
+});
+
+app.get('/about', function(req, res){
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+})
+
+app.listen(3000, function(){
+  console.log('Server is up on port 3000');
+});
